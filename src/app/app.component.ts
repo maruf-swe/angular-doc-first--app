a47c4818ecb43d03deea2b51d79df26b9dd975e7
maruf-swe <marufur1847@gmail.com>
2023-12-20 05:59:17 +0600

    Create first-app project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: '<h1>Hello World</h1>',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'homes';
+}
