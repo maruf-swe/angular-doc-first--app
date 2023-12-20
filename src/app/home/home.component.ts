823bd710a8da1b9abd7e6949334ba33e38b71447
maruf-swe <marufur1847@gmail.com>
2023-12-20 07:18:21 +0600

    add housinglocation interface data types

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -22,5 +23,18 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+
+  };
 
 }
---
+++ src/app/housinglocation.ts
@@ -0,0 +1,10 @@
+export interface Housinglocation {
+    id :number;
+    name: String;
+    city: String;
+    state: String;
+    photo :String;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
+}
