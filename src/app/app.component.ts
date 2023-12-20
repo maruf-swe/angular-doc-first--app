707cacd3e6bc532c049261c16cb70cb66358b818
maruf-swe <marufur1847@gmail.com>
2023-12-20 06:03:33 +0600

    Create Home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent],
   template: '<h1>Hello World</h1>',
   styleUrl: './app.component.css'
 })
