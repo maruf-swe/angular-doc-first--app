dc741dab87fedf387183ee947e1ec82ebb83241d
maruf-swe <marufur1847@gmail.com>
2023-12-20 06:48:41 +0600

    add home css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,13 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>  
-  `,
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+`,
+
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
