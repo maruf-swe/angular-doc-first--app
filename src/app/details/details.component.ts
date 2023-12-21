7e10f5dcae2e7022ad8404f49d228c14d2166413
maruf-swe <marufur1847@gmail.com>
2023-12-22 05:45:18 +0600

    add router link in single item

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo" 
@@ -22,7 +24,11 @@ import { HousingLocation } from '../housinglocation';
           <li>Units available: {{housingLocation?.availableUnits}}</li>
           <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
           <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
-      </ul>
+        </ul>
+      </section> 
+
+      <section class="listing">
+        <a [routerLink]="['']"><b>Back to Home</b></a>
       </section>
     </article>
   `,
